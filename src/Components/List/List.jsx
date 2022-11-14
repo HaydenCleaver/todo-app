2c31cdb6c78ac62dbdfa25d1947460e78d5cda5b
Hayden Cleaver <rexcleaver90@gmail.com>
2022-11-14 07:50:29 +0000

    items render

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -1,6 +1,7 @@
 import { Pagination, Card, createStyles, Badge, Text, Group, CloseButton} from '@mantine/core';
 import {When} from 'react-if';
 import {useState, useContext} from 'react';
+import { AuthContext } from '../../Context/Auth/Auth';
 import {SettingsContext} from '../../Context/Settings/Settings';
 import Auth from '../Auth/Auth';
 
@@ -12,10 +13,11 @@ const useStyles = createStyles((theme) => ({
   }
 }))
 
-const List = ({list, toggleComplete, deleteItem}) => {
+const List = ({capability, list, toggleComplete, deleteItem}) => {
 
   const {classes} = useStyles();
 
+  const { isLoggedIn } = useContext(AuthContext);
   const{itemNumber, hideStatus} = useContext(SettingsContext);
   const[page, setPage] = useState(1);
 
@@ -26,29 +28,33 @@ const List = ({list, toggleComplete, deleteItem}) => {
 
   const pageCount = Math.ceil(paginationList.length / itemNumber);
   const displayList = paginationList.slice(listStart, listEnd);
+  
   return(
     <>
       {displayList.map(item => (
-        <Card key={item.id} withBorder>
-          <Card.Section withBorder>
-            <Group position='apart'>
-              <Group position='left'>
-                <Badge 
-                  className={classes.badge} 
-                  color={item.complete ? "green" : "red"} 
-                  variant="filled" onClick={()=> toggleComplete(item.id)}
-                  > {item.complete ? "Complete" : "Pending"}
-                </Badge>
-                <Text>{item.assignee}</Text>
+        <When condition={isLoggedIn}>
+
+          <Card key={item.id} withBorder>
+            <Card.Section withBorder>
+              <Group position='apart'>
+                <Group position='left'>
+                  <Badge 
+                    className={classes.badge} 
+                    color={item.complete ? "green" : "red"} 
+                    variant="filled" onClick={()=> toggleComplete(item.id)}
+                    > {item.complete ? "Complete" : "Pending"}
+                  </Badge>
+                  <Text>{item.assignee}</Text>
+                </Group>
+                <Auth>
+                  <CloseButton title="delete item" onClick={()=> deleteItem(item.id)}/>
+                </Auth>
               </Group>
-              <Auth>
-                <CloseButton title="delete item" onClick={()=> deleteItem(item.id)}/>
-              </Auth>
-            </Group>
-          </Card.Section>
-          <Text align="left">{item.text}</Text>
-          <Text align="right">Difficulty: {item.difficulty}</Text>
-        </Card>
+            </Card.Section>
+            <Text align="left">{item.text}</Text>
+            <Text align="right">Difficulty: {item.difficulty}</Text>
+          </Card>
+        </When>
       ))}
       <When condition={paginationList.length > 0}>
         <Pagination page={page} onChange={setPage} total={pageCount}/>
